530dd5e9624277427cf1af173e723f82b705f2a3
Gustavo <gustavo.novaes@aluno.ufabc.edu.br>
2021-06-10 10:21:15 -0300

    Task 7 - Atualização Model, Repository e Controller

---
+++ Task 7/alicia/src/main/java/org/generation/alicia/model/Usuario.java
@@ -0,0 +1,149 @@
+package org.generation.alicia.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+	private Date data_nascimento;
+	
+	@NotNull
+	@Email
+	private String login;
+	
+	@NotNull
+	@Size(min=5)
+	private String senha;
+	
+	@Size(max=100)
+	private String nomeStartUp;
+	
+	@Size(min= 5, max=500)
+	private String resumo;
+	
+	private String linkLinkedin;
+	
+	private String linkInstagram;
+	
+	private String linkFacebook;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getNomeStartUp() {
+		return nomeStartUp;
+	}
+
+	public void setNomeStartUp(String nomeStartUp) {
+		this.nomeStartUp = nomeStartUp;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public String getLinkLinkedin() {
+		return linkLinkedin;
+	}
+
+	public void setLinkLinkedin(String linkLinkedin) {
+		this.linkLinkedin = linkLinkedin;
+	}
+
+	public String getLinkInstagram() {
+		return linkInstagram;
+	}
+
+	public void setLinkInstagram(String linkInstagram) {
+		this.linkInstagram = linkInstagram;
+	}
+
+	public String getLinkFacebook() {
+		return linkFacebook;
+	}
+
+	public void setLinkFacebook(String linkFacebook) {
+		this.linkFacebook = linkFacebook;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+}
